50974c77ea35749c2b19e42af160983b688b7eb6
Fabio <alessf13@gmail.com>
2021-08-29 17:22:58 +0200

    feat: create DB and connect via StudentRepository

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -7,15 +8,15 @@ import java.util.List;
 
 @Service
 public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public  StudentService(StudentRepository studentRepository){
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents() {
-        return List.of(
-                new Student(
-                        1L,
-                        "Jane",
-                        "jane@test.com",
-                        LocalDate.of(2000, 1, 5),
-                        21
-                )
-        );
+      return studentRepository.findAll();
     }
 }
